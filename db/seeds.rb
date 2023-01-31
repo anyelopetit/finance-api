d10c5d0604698574acf05697ba8083ac08c0ee2a
Anyelo Petit <anyelopetit@gmail.com>
2023-01-31 18:10:56 -0400

    Preparing to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Categories
+Category.create!(name: 'Food & Drinks')
+Category.create!(name: 'Shopping')
+Category.create!(name: 'Housing')
+Category.create!(name: 'Transportation')
+Category.create!(name: 'Vehicle')
+Category.create!(name: 'Life & Entertainment')
+Category.create!(name: 'Communication, PC')
+Category.create!(name: 'Financial expenses')
+Category.create!(name: 'Investments')
+Category.create!(name: 'Income')
+Category.create!(name: 'Others')
